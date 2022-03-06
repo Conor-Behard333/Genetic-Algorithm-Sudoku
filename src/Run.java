347c24d1919f419b6db1b715c5168377d93c5f14
Conor Bèhard Roberts <47724661+Conor-Behard333@users.noreply.github.com>
2022-03-06 19:55:35 +0000

    working fitness function

--- src/Run.java
+++ src/Run.java
@@ -1,7 +1,8 @@
-class Run{
-    public static void main(String[] args) {
-        Board b = new Board();
-        System.out.println(b);
+class Run {
 
-    }
+  public static void main(String[] args) {
+    // The higher the better
+    // A fitness score of 243 is a solution
+    System.out.println(FitnessCalc.calculateFitness(new Individual()));
+  }
 }
