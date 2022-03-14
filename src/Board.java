7bd8ef91cc4eaaabfda46515e25b45c945cabf6d
Conor Bèhard Roberts <47724661+Conor-Behard333@users.noreply.github.com>
2022-03-14 11:12:02 +0000

    represent as 1d

--- src/Board.java
+++ src/Board.java
@@ -1,101 +1,110 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
 
 public class Board {
 
-  private ArrayList<int[]> allowedToChange = new ArrayList<>();
-  private int[][] startingBoard;
-
-  private int[][] board;
-
-  private int[] gene;
+  private ArrayList<Integer> allowedToChange = new ArrayList<>();
+  private int[] startingBoard;
+  private int[] chromosome;
 
   Board(int[][] startingBoard, boolean child) {
-    this.startingBoard = startingBoard;
+    this.startingBoard = flatten(startingBoard);
 
     //copy starting board into board
-    board = Arrays.stream(startingBoard).map(a ->  Arrays.copyOf(a, a.length)).toArray(int[][]::new);
+    chromosome = Arrays.copyOf(this.startingBoard, this.startingBoard.length);
 
-    int count = 0;
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j] == 0) {
-          allowedToChange.add(new int[] { i, j });
-          count++;
-        }
+    for (int i = 0; i < chromosome.length; i++) {
+      if (chromosome[i] == 0) {
+        allowedToChange.add(i);
       }
     }
-    
 
-    Random rand = new Random();
-    gene = new int[count];
-    int index = 0;
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j] == 0) {
-          if (child == true) {
-            gene[index] = 0;
-            index++;
-          } else {
-            gene[index] = rand.nextInt((9 - 1) + 1) + 1;
-            index++;
+    for (int i = 0; i < chromosome.length; i++) {
+      if (chromosome[i] == 0) {
+        if (child == false) {
+          int randomNum = Rand.randomInt(9, 1);
+          while (randomNumExists(chromosome, randomNum, i)) {
+            randomNum = Rand.randomInt(9, 1);
           }
+          chromosome[i] = randomNum;
         }
       }
     }
+  }
+
+  private boolean randomNumExists(int[] chromosome, int randomNum, int index) {
+    int start = 9 * (index / 9);
+    int end = start + 9;
+    for (int i = start; i < end; i++) {
+      if (chromosome[i] == randomNum) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+/**
+     * Converts a 2d array into a 1d array
+     * 
+     * @param arr: Array to be flattened 
+     * @return the 1d version of the given 2d array
+     */
+  private int[] flatten(int[][] arr) {
+    int[] flat = new int[arr.length * arr[0].length];
+    int index = 0;
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr.length; j++) {
+        flat[index] = arr[i][j];
+        index++;
+      }
+    }
+    return flat;
+  }
+    
+  public int[][] getTwoDChromosome() {
+      int[][] twoD = new int[9][9];
+      int index = 0;
+      for (int i = 0; i < twoD.length; i++) {
+          for (int j = 0; j < twoD.length; j++) {
+              twoD[i][j] = chromosome[index];
+              index++;
+          }
+      }
+      return twoD;
   }
 
   /**
     * @return the board
     */
-  public int[][] getBoard() {
-    return board;
+  public int[] getChromosome() {
+    return chromosome;
   }
 
   /**
-    * @param gene the gene to set
+    * @param Chromosome the Chromosome to set
     */
-  public void setGene(int[] gene) {
-    this.gene = gene;
-  }
-  
-  /**
-    * @return the gene
-    */
-  public int[] getGene() {
-      return gene;
+  public void setChromosome(int[] Chromosome) {
+    this.chromosome = Chromosome;
   }
 
   @Override
   public String toString() {
-    create();
-    StringBuilder result = new StringBuilder();
-    for (int i = 0; i < board.length; i++) {
-      result.append(Arrays.toString(board[i]) + "\n");
-    }
-    reset();
-    return result.toString();
-  }
-
-  public boolean isAllowedToChane(int row, int col) {
-    return allowedToChange.contains(new int[] { row, col });
-  }
-
-  public void create() {
-    int index = 0;
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j] == 0) {
-            board[i][j] = gene[index];
-            index++;
-        }
+      StringBuilder result = new StringBuilder();
+      for (int i = 0; i < chromosome.length; i++) {
+          result.append(chromosome[i] + ", ");
+          if ((i + 1) % 9 == 0) {
+              result.append("\n");
+          }
       }
-    }
+      return result.toString();
   }
 
-  public void reset() {
-    board = Arrays.stream(startingBoard).map(a -> Arrays.copyOf(a, a.length)).toArray(int[][]::new);
+  public boolean isAllowedToChange(int index) {
+    return allowedToChange.contains(index);
   }
+
+  // public void reset() {
+    // chromosome = Arrays.stream(startingBoard).map(a -> Arrays.copyOf(a, a.length)).toArray(int[][]::new);
+  // }
   
 }
